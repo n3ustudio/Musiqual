730620c4c7b547f1b5704f843a6e642d49481b39
Il Harper <afanyiyu@hotmail.com>
2020-03-09 15:35:42 +0800

    feat: ParameterControl Changes

--- Musiqual/Parameter/Controls/ParameterControl.xaml.cs
+++ Musiqual/Parameter/Controls/ParameterControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Scrosser.Models;
 
 namespace Musiqual.Parameter.Controls
 {
@@ -20,9 +21,42 @@ namespace Musiqual.Parameter.Controls
     /// </summary>
     public partial class ParameterControl : UserControl
     {
+
         public ParameterControl()
         {
             InitializeComponent();
         }
+
+        #region DependencyProperty
+
+        public static readonly DependencyProperty ParameterDataProperty = DependencyProperty.Register(
+            "ParameterData", typeof(ParameterData), typeof(ParameterControl), new PropertyMetadata(default(ParameterData)));
+
+        public ParameterData ParameterData
+        {
+            get { return (ParameterData)GetValue(ParameterDataProperty); }
+            set { SetValue(ParameterDataProperty, value); }
+        }
+
+        public static readonly DependencyProperty VerticalScrossProperty = DependencyProperty.Register(
+            "VerticalScross", typeof(Scross), typeof(ParameterControl), new PropertyMetadata(default(Scross)));
+
+        public Scross VerticalScross
+        {
+            get { return (Scross)GetValue(VerticalScrossProperty); }
+            set { SetValue(VerticalScrossProperty, value); }
+        }
+
+        public static readonly DependencyProperty HorizontalScrossProperty = DependencyProperty.Register(
+            "HorizontalScross", typeof(Scross), typeof(ParameterControl), new PropertyMetadata(default(Scross)));
+
+        public Scross HorizontalScross
+        {
+            get { return (Scross)GetValue(HorizontalScrossProperty); }
+            set { SetValue(HorizontalScrossProperty, value); }
+        }
+
+        #endregion
+
     }
 }
