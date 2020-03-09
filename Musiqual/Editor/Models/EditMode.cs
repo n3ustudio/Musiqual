75725a5a5e11acb743282cde10ac642c28c100bd
Il Harper <afanyiyu@hotmail.com>
2020-03-09 11:11:02 +0800

    feat: Add EditMode Enum

---
+++ Musiqual/Editor/Models/EditMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musiqual.Editor.Models
+{
+    public enum EditMode
+    {
+        Arrow = 0,
+        Pencil = 1,
+        Eraser = 2,
+        Scissors = 3,
+        Glue = 4,
+        Stretch = 5,
+        Playback = 6
+    }
+}
