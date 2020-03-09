b4a9da13d430b16f6df0d4f40b789bab234fdf35
Il Harper <afanyiyu@hotmail.com>
2020-03-09 15:35:08 +0800

    feat: EditMode Class Changes

--- Musiqual/Editor/Models/EditMode.cs
+++ Musiqual/Editor/Models/EditMode.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Musiqual.Editor.Models
 {
-    public enum EditMode
+
+    public class EditMode : INotifyPropertyChanged
+    {
+
+        EditMode(EditModeEnum mode = 0)
+        {
+            Mode = mode;
+        }
+
+        private EditModeEnum _mode = 0;
+
+        public EditModeEnum Mode
+        {
+            get => _mode;
+            set
+            {
+                _mode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public enum EditModeEnum
     {
         Arrow = 0,
         Pencil = 1,
@@ -16,4 +47,5 @@ namespace Musiqual.Editor.Models
         Stretch = 5,
         Playback = 6
     }
+
 }
