b0fff17f1767b59338e0e45b45c55246c7b18aa1
Il Harper <afanyiyu@hotmail.com>
2020-03-10 10:20:51 +0800

    feat: ParameterData Changes

--- Musiqual/Parameter/ParameterData.cs
+++ Musiqual/Parameter/ParameterData.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Musiqual.Parameter
 {
-    public class ParameterData
+
+    /// <summary>
+    /// The IParameterData interface.
+    /// </summary>
+    public interface IParameterData
     {
     }
+
+    /// <summary>
+    /// The positive/negative parameter data.
+    /// </summary>
+    public class NormalParameterData : IParameterData
+    {
+
+    }
+
+    /// <summary>
+    /// The positive parameter data.
+    /// </summary>
+    public class NaturalParameterData : IParameterData
+    {
+
+    }
+
 }
