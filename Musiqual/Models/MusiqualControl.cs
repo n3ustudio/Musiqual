1cd1747c276753afc107095a60c494e000818ed5
Il Harper <afanyiyu@hotmail.com>
2020-03-10 23:09:22 +0800

    feat: Add Parameter & MusiqualControl

---
+++ Musiqual/Models/MusiqualControl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Musiqual.Models
+{
+
+    public abstract class MusiqualControl : Control, INotifyPropertyChanged
+    {
+
+        public MusiqualControl(double position)
+        {
+            _position = position;
+        }
+
+        #region DataContext
+
+        private double _position;
+
+        public double Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Musiqual/Models/Parameter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musiqual.Models
+{
+
+    public sealed class Parameter : MusiqualControl
+    {
+
+        public Parameter(int position, double value)
+            : base(position)
+        {
+            _value = value;
+        }
+
+        #region DataContext
+
+        private double _value;
+
+        public double Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+
+}
