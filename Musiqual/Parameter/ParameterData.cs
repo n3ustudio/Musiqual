289be03cbdda96010e399f17c9f6db8b5109e715
Il Harper <afanyiyu@hotmail.com>
2020-03-10 11:01:03 +0800

    feat: ParameterData Changes

--- Musiqual/Parameter/ParameterData.cs
+++ Musiqual/Parameter/ParameterData.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +11,9 @@ namespace Musiqual.Parameter
 {
 
     /// <summary>
-    /// The IParameterData interface.
+    /// The ParameterData interface.
     /// </summary>
-    public interface IParameterData
+    public interface IParameterData : INotifyPropertyChanged
     {
 
         bool IsNatural { get; }
@@ -18,22 +21,51 @@ namespace Musiqual.Parameter
     }
 
     /// <summary>
-    /// The positive/negative parameter data.
+    /// The ParameterData base class.
     /// </summary>
-    public abstract class NormalParameterData : IParameterData
+    /// <typeparam name="T">The parameter type.</typeparam>
+    public abstract class ParameterData<T> : IParameterData
     {
 
-        public bool IsNatural => false;
+        public bool IsNatural { get; }
+
+        private ObservableCollection<T> _parameterList = new ObservableCollection<T>();
+
+        public ObservableCollection<T> ParameterList
+        {
+            get => _parameterList;
+            set
+            {
+                _parameterList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    /// <summary>
+    /// The positive/negative parameter data.
+    /// </summary>
+    public abstract class NormalParameterData<T> : ParameterData<T>
+    {
+
+        public new bool IsNatural => false;
 
     }
 
     /// <summary>
     /// The positive parameter data.
     /// </summary>
-    public abstract class NaturalParameterData : IParameterData
+    public abstract class NaturalParameterData<T> : ParameterData<T>
     {
 
-        public bool IsNatural => true;
+        public new bool IsNatural => true;
 
     }
 
@@ -41,7 +73,7 @@ namespace Musiqual.Parameter
     /// The placeholder parameter data.
     /// NO NOT USE THIS CLASS AS PARAMETER UNLESS YOU KNOW WHAT YOU ARE DOING.
     /// </summary>
-    public sealed class PlaceHolderParameterData : NormalParameterData
+    public sealed class PlaceHolderParameterData : NormalParameterData<double>
     {
 
     }
