815671e213320bfd41f32a2630c8bc1d20c26cef
Il Harper <afanyiyu@hotmail.com>
2020-03-18 00:11:32 +0800

    feat: AutoScrollView Complete

--- Musiqual/Parameter/Views/AutoScrollView.xaml.cs
+++ Musiqual/Parameter/Views/AutoScrollView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +14,61 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using YDock.Interface;
 
 namespace Musiqual.Parameter.Views
 {
     /// <summary>
     /// AutoScrollView.xaml 的交互逻辑
     /// </summary>
-    public partial class AutoScrollView : UserControl
+    public partial class AutoScrollView : UserControl, INotifyPropertyChanged, IDockSource
     {
+
         public AutoScrollView()
         {
             InitializeComponent();
         }
+
+        #region Current
+
+        public static AutoScrollView Current { get; } = new AutoScrollView();
+
+        #endregion
+
+        #region DataContext
+
+        private bool _isAutoScrollEnabled;
+
+        public bool IsAutoScrollEnabled
+        {
+            get => _isAutoScrollEnabled;
+            set
+            {
+                _isAutoScrollEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region DockSource
+
+        public IDockControl DockControl { get; set; }
+        public string Header => "Auto Scroll";
+        public ImageSource Icon => null;
+
+        #endregion
+
     }
 }
